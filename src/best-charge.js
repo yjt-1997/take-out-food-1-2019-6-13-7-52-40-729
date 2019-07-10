6cc74a98e4c2fc8a5a68fab764c8692ef9bb6e01
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-10 20:50:46 +0800

    commit tasking diagram

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,26 @@
+const items = loadAllItems();
+const promotions = loadPromotions();
 function bestCharge(selectedItems) {
   return /*TODO*/;
+}
+function isValid(inputs) {
+  let isValid = true;
+  let isExsit = false;
+  for (let i = 0; i < inputs.length; i++) {
+    let inputItem = element.split("x");
+    let itemId = inputItem[0].trim();
+    if (!/^ITEM00[0-9]{2}$/.test(itemId)) {
+      isValid = false;
+      break;
+    }
+    for (let j = 0; j < items.length; j++) {
+      if (itemId == items[j].id) {
+        isExsit = true;
+        break;
+      }
+    }
+    if (!isExsit || !isValid)
+      return false;
+  }
+  return isValid;
 }
